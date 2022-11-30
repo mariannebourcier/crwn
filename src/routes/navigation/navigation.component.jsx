1ef9e5731a04189939c9cebec8e29d4e5cfccea0
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-30 14:25:38 -0500

    navigation, router, firebase

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,36 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+       <div className='navigation'>
+         <Link className='logo-container' to='/'>
+         <CrwnLogo className='logo' />
+         </Link>
+      
+       <div className='nav-links-container'>
+         <Link className='nav-link' to='/shop'>
+          SHOP
+         </Link>
+         <Link className='nav-link' to='/signin'>
+          SIGN IN
+         </Link>
+      </div>
+      </div>
+      
+      <Outlet />
+    </Fragment>
+     
+   
+  )
+}
+
+export default Navigation;
+
+//fragment act as wrapping div without crowding the dom
+//link = anchor tag inside browser router
+//svg = scalable logo
+//box-sizing:border-box; to make css border padding properties dont get thrown off by adding too much size
