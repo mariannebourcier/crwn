5f73cc302728bbccff89f63bfdc3f5ff57879c27
mariannebourcier <marianne.bourcier@gmail.com>
2022-12-30 14:31:12 -0700

    add functionality for adding items to carts

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,15 +1,40 @@
 import { createContext , useState} from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cart items contain producttoadd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id)
+
+  // if found, increment quantity
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) => 
+      cartItem.id === productToAdd.id 
+      ? {...cartItem, quantity: cartItem.quantity + 1}
+      : cartItem
+    )}
+  
+  // return new array with modified cart items/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1}]
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => {}
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {}
 })
-
+ 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([])
 
-  const value = {isCartOpen, setIsCartOpen}
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd))
+
+  }
+
+  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems}
 
 
   return (
