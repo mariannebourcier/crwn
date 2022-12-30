f639cc2065e9bb48ae0eff755156401e72640957
mariannebourcier <marianne.bourcier@gmail.com>
2022-12-30 15:16:03 -0700

    add addtocart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext , useState} from "react";
+import { createContext , useEffect, useState} from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   // find if cart items contain producttoadd
@@ -22,19 +22,27 @@ export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
-  addItemToCart: () => {}
+  addItemToCart: () => {},
+  cartCount: 0
 })
  
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
+  const [cartCount, setCartCount] = useState(0)
+
+  // everytime something changes in cartItems , recalculate cart count
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount)
+  }, [cartItems])
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd))
 
   }
 
-  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems}
+  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount}
 
 
   return (
